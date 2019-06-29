8ba5ce5fa891aecc5a92903739e8879ccfc79312
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-29 19:44:00 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+RSA="\033[1;31m"
+YSA="\033[1;93m"
+
+WHO="$( whoami )"
+
+if [[ "$WHO" != "root" ]]
+then
+sleep 1
+echo -e "$RSA"run it as"$CE" "$YSA"root"$CE"
+sleep 1
+echo -e "$RSA"or use"$CE" "$YSA"sudo"$CE"
+sleep 1
+exit
+fi
+
+if [[ -d ~/mouse ]]
+then
+cd  ~/mouse
+{
+cp bin/mouse /usr/local/bin
+chmod +x /usr/local/bin/mouse
+cp bin/mouse /bin
+chmod +x /bin/mouse
+} &> /dev/null
+else
+cd ~
+{
+git clone https://github.com/entynetproject/mouse.git
+cd  ~/mouse
+cp bin/mouse /usr/local/bin
+chmod +x /usr/local/bin/mouse
+cp bin/mouse /bin
+chmod +x /bin/mouse
+} &> /dev/null
+fi
