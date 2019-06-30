d45a29abbf3baa9d36b899f7c2f086c44c746ca5
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 19:54:04 +0300

    Update server.py

--- modules/server.py
+++ modules/server.py
@@ -42,7 +42,7 @@ class Server:
         elif device_type == "iOS":
             result = self.modules_ios
         elif device_type == "Linux":
-            self.craft_payload
+            os.system("python resources/mpl.py")
 
     def set_host_port(self):
         try:
