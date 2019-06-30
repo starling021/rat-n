604077997267893ba3c96ffee6195e8402775984
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 18:22:03 +0300

    Update vibrate_ios.py

--- modules/commands/iOS/vibrate_ios.py
+++ modules/commands/iOS/vibrate_ios.py
@@ -1,7 +1,7 @@
 class command:
 	def __init__(self):
 		self.name = "vibrate"
-		self.description = "vibrate device"
+		self.description = "Vibrate device."
 
 	def run(self,session,cmd_data):
 		session.send_command(cmd_data)
