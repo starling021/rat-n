9ec9bd007de610fc138592fa47d6153d1c09e9a2
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 18:18:42 +0300

    Update safemode_ios.py

--- modules/commands/iOS/safemode_ios.py
+++ modules/commands/iOS/safemode_ios.py
@@ -1,7 +1,7 @@
 class command:
     def __init__(self):
         self.name = "safemode"
-        self.description = "put device into safe mode"
+        self.description = "Put device into safe mode."
 
     def run(self,session,cmd_data):
     	cmd_data["cmd"] = ";"
