1d2c3bf01eb47a35156ae95bd62cc16736ff6166
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 17:30:53 +0300

    Update pid_universal.py

--- modules/commands/universal/pid_universal.py
+++ modules/commands/universal/pid_universal.py
@@ -1,7 +1,7 @@
 class command:
     def __init__(self):
         self.name = "pid"
-        self.description = "get process id"
+        self.description = "Get MPL process id."
         self.type = "native"
 
     def run(self,session,cmd_data):
