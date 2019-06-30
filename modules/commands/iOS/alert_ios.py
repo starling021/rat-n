ecaf645296cf13ab5e4b748ff21221779b67829a
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 17:54:16 +0300

    Update alert_ios.py

--- modules/commands/iOS/alert_ios.py
+++ modules/commands/iOS/alert_ios.py
@@ -4,11 +4,11 @@ import json
 class command:
     def __init__(self):
         self.name = "alert"
-        self.description = "make alert show up on device"
+        self.description = "Make alert show up on device."
         self.type = "native"
 
     def run(self,session,cmd_data):
-        title = raw_input("title: ")
-        message = raw_input("message: ")
+        title = raw_input("Title: ")
+        message = raw_input("Message: ")
         session.send_command({"cmd":"alert","args":json.dumps({"title":title,"message":message})})
         return ""
