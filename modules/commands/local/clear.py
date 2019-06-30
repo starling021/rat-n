a7de767beb45dc30e7ff9a7d431fe608a8e30e63
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 17:33:20 +0300

    Update clear.py

--- modules/commands/local/clear.py
+++ modules/commands/local/clear.py
@@ -3,7 +3,7 @@ import os
 class command:
     def __init__(self):
         self.name = "clear"
-        self.description = "clear terminal"
+        self.description = "Clean terminal input/output."
     
     def run(self,session,cmd_data):
         os.system('clear')
