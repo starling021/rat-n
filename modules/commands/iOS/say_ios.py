0999afcd219ea1ec60ef4ba7c9c7d35514526701
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 18:19:38 +0300

    Update say_ios.py

--- modules/commands/iOS/say_ios.py
+++ modules/commands/iOS/say_ios.py
@@ -1,8 +1,8 @@
 class command:
     def __init__(self):
         self.name = "say"
-        self.description = "text to speach"
-        self.usage = "Usage: say hello"
+        self.description = "Text to speach."
+        self.usage = "Usage: say <text>"
     
     def run(self,session,cmd_data):
     	if not cmd_data['args']:
