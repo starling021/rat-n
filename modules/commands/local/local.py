a1d1326cfb11253599911760b74d21131adee82f
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 17:33:58 +0300

    Update local.py

--- modules/commands/local/local.py
+++ modules/commands/local/local.py
@@ -4,11 +4,11 @@ from os.path import expanduser
 class command:
     def __init__(self):
         self.name = "local"
-        self.description = "Run local shell commands"
+        self.description = "Run local shell commands."
     
     def run(self,session,cmd_data):
         if not cmd_data['args']:
-            print "Usage: local shell commands"
+            print "Usage: local <command>"
             return
         else:
             split_args = cmd_data['args'].split()
