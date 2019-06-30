bc7dc821d0ddc3a687f646155c40522292c0c430
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 18:20:21 +0300

    Update setvol.py

--- modules/commands/iOS/setvol.py
+++ modules/commands/iOS/setvol.py
@@ -1,8 +1,8 @@
 class command:
     def __init__(self):
         self.name = "setvol"
-        self.description = "set device volume"
-        self.usage = "Usage: volume 1.0"
+        self.description = "Set device volume."
+        self.usage = "Usage: volume <level: 0.0>"
     
     def run(self,session,cmd_data):
     	if not cmd_data['args']:
