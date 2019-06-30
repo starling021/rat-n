01ac4ce6ec7a3ad499e160fe0b763f8100358e8e
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 17:49:56 +0300

    Update setvol_macos.py

--- modules/commands/macOS/setvol_macos.py
+++ modules/commands/macOS/setvol_macos.py
@@ -1,11 +1,11 @@
 class command:
     def __init__(self):
         self.name = "setvol"
-        self.description = "set output volume"
+        self.description = "Set output volume."
      
     def run(self,session,cmd_data):
         if not cmd_data['args']:
-            print "Usage: setvol 0-100"
+            print "Usage: setvol <level: 0-100>"
             return -1
         payload = "set volume output volume "+cmd_data['args']
         cmd_data.update({"cmd":"applescript","args":payload})
