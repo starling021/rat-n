a84f3c705cf4fdcc582d0979a80a4537f6a3eb63
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 18:03:07 +0300

    Update home_ios.py

--- modules/commands/iOS/home_ios.py
+++ modules/commands/iOS/home_ios.py
@@ -1,7 +1,7 @@
 class command:
     def __init__(self):
         self.name = "home"
-        self.description = "simulate a home button press"
+        self.description = "Simulate a home button press."
     
     def run(self,session,cmd_data):
         error = session.send_command(cmd_data)
