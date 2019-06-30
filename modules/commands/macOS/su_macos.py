09cac419fc73ef9e1b0e6daa274c2e21ff6957ed
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 17:52:52 +0300

    Update su_macos.py

--- modules/commands/macOS/su_macos.py
+++ modules/commands/macOS/su_macos.py
@@ -5,7 +5,7 @@ import modules.helper as h
 class command:
     def __init__(self):
         self.name = "su"
-        self.description = "su login"
+        self.description = "Login as root."
         self.type = "eggsu"
 
     def run(self,session,cmd_data):
@@ -15,12 +15,12 @@ class command:
         cmd_data['cmd'] = "eggsu"
         result = session.send_command(cmd_data)
         if "root" in result:
-            h.info_general("Root Granted")
+            h.info_general("Root Granted!")
             time.sleep(0.2)
-            h.info_general("Escalating Privileges")
+            h.info_general("Escalating Privileges...")
             if session.server.is_multi == False:
                 session.server.update_session(session)
             else:
                 session.needs_refresh = True
         else:
-            print "failed getting root"
+            h.info_error("Failed getting root!")
