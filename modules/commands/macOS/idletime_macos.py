39fe7ba21ed902a65fbab35e1d350203ad51af77
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 17:38:01 +0300

    Update idletime_macos.py

--- modules/commands/macOS/idletime_macos.py
+++ modules/commands/macOS/idletime_macos.py
@@ -1,7 +1,7 @@
 class command:
     def __init__(self):
         self.name = "idletime"
-        self.description = "get the amount of time since the keyboard/cursor were touched"
+        self.description = "Get the amount of time since the keyboard/cursor were touched."
         self.type = "native"
 
     def run(self,session,cmd_data):
