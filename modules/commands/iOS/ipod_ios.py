22d07bad97116fe35dd61792443a9a45838bc15c
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 18:06:45 +0300

    Update ipod_ios.py

--- modules/commands/iOS/ipod_ios.py
+++ modules/commands/iOS/ipod_ios.py
@@ -1,8 +1,8 @@
 class command:
     def __init__(self):
         self.name = "ipod"
-        self.description = "control music player"
-        self.usage = "Usage: ipod play|pause|next|prev|info"
+        self.description = "Control music player."
+        self.usage = "Usage: ipod <play|pause|next|prev|info>"
     
     def run(self,session,cmd_data):
     	if not cmd_data['args'] or not cmd_data['args'] in ['play','pause','next','prev','info']:
