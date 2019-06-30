18d850fe3ca6ec767f26d284fb8eee2f01f62fd1
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 13:00:51 +0300

    Add files via upload

--- mouse.py
+++ mouse.py
@@ -30,24 +30,25 @@ class mouse:
                           (((-'    __//
                                  (((-'
 """+h.WHITE+"Public Version: "+h.GREEN_THIN+"mouse-v1.6-"+SAS+h.WHITE+"\nDeveloped by Entynetproject (Ivan Nikolsky)\n"+h.ENDC
-        self.main_menu_text = h.WHITE+"-"*40+""""""
+        self.main_menu_text = h.WHITE+"-"*40+"""
 
-print(h.GREEN_THIN+"1"+h.WHITE+") Start Server")
-print(h.GREEN_THIN+"2"+h.WHITE+") Start MultiHandler")
-print(h.GREEN_THIN+"3"+h.WHITE+") Create Payload")
-print(h.GREEN_THIN+"0"+h.WHITE+") Exit")
-print(h.NES)
-      
+    """+h.GREEN_THIN+"""1"""+h.WHITE+""") Start Server
+    """+h.GREEN_THIN+"""2"""+h.WHITE+""") Start MultiHandler
+    """+h.GREEN_THIN+"""3"""+h.WHITE+""") Create Payload
+    """+h.GREEN_THIN+"""0"""+h.WHITE+""") Exit
+
+"""+h.NES
+
 
     # Actions
-def print_payload(self,payload,number_option):
-    print (h.GREEN_THIN+" " * 4 + str(number_option) +h.WHITE+") "+ payload.name)
+    def print_payload(self,payload,number_option):
+        print (h.GREEN_THIN+" " * 4 + str(number_option) +h.WHITE+") "+ payload.name)
     
 
 
-def start_single_server(self):
-    os.system("clear")
-    print(h.WHITE+"""
+    def start_single_server(self):
+        os.system("clear")
+        print(h.WHITE+"""
                _     __,..---""-._                 ';-,
         ,    _/_),-"`             '-.                `\\
        \|.-"`    -_)                 '.                ||
@@ -60,16 +61,16 @@ def start_single_server(self):
                           (((-'    __//
                                  (((-'
 """+h.WHITE+"Public Version: "+h.GREEN_THIN+"mouse-v1.6-"+SAS+h.WHITE+"\nDeveloped by Entynetproject (Ivan Nikolsky)"+h.ENDC)
-    print(h.WHITE+"-"*40)
-    print("")
-    if not self.server.set_host_port():
-        return
-    self.server.start_single_handler()
+        print(h.WHITE+"-"*40)
+        print("")
+        if not self.server.set_host_port():
+            return
+        self.server.start_single_handler()
 
 
-def start_multi_handler(self):
-    os.system("clear")
-    print(h.WHITE+"""
+    def start_multi_handler(self):
+        os.system("clear")
+        print(h.WHITE+"""
                _     __,..---""-._                 ';-,
         ,    _/_),-"`             '-.                `\\
        \|.-"`    -_)                 '.                ||
@@ -82,130 +83,130 @@ def start_multi_handler(self):
                           (((-'    __//
                                  (((-'
 """+h.WHITE+"Public Version: "+h.GREEN_THIN+"mouse-v1.6-"+SAS+h.WHITE+"\nDeveloped by Entynetproject (Ivan Nikolsky)"+h.ENDC)
-    print(h.WHITE+"-"*40)
-    print("")
-    if not self.server.set_host_port():
-        return
-    self.server.start_multi_handler()
+        print(h.WHITE+"-"*40)
+        print("")
+        if not self.server.set_host_port():
+            return
+        self.server.start_multi_handler()
 
 
-def prompt_run_server(self):
-    if raw_input(h.NES+"Start Server? (Y/n): ") == "n":
-        return
-    else:
-        if raw_input(h.NES+"MultiHandler? (y/N): ") == "y":
-            self.server.start_multi_handler()
+    def prompt_run_server(self):
+        if raw_input(h.NES+"Start Server? (Y/n): ") == "n":
+            return
         else:
-            self.server.start_single_handler()
+            if raw_input(h.NES+"MultiHandler? (y/N): ") == "y":
+                self.server.start_multi_handler()
+            else:
+                self.server.start_single_handler()
 
 
-def import_payloads(self):
-    path = "modules/payloads"
-    sys.path.append(path)
-    modules = dict()
-    for mod in os.listdir(path):
-        if mod == '__init__.py' or mod[-3:] != '.py':
-            continue
-        else:
-            m = __import__(mod[:-3]).payload()
-            modules[m.name] = m
-    return modules
-
-
-def exit_menu(self):
-    os.system("clear")
-    print(h.WHITE+"""
-               _     __,..---""-._                 ';-,
-        ,    _/_),-"`             '-.                `\\
-       \|.-"`    -_)                 '.                ||
-       /`   a   ,                      \              .'/
-       '.___,__/                 .-'    \_        _.-'.'
-          |\  \      \         /`        _`""""""`_.-'
-             _/;--._, >        |   --.__/ `""""""`
-           (((-'  __//`'-......-;\      )
-                (((-'       __//  '--. /   Mouse/RAT
-                          (((-'    __//
-                                 (((-'
-"""+h.WHITE+"Public Version: "+h.GREEN_THIN+"mouse-v1.6-"+SAS+h.WHITE+"\nDeveloped by Entynetproject (Ivan Nikolsky)"+h.ENDC)
-    print(h.WHITE+"-"*40)
-    print("")
-    exit()
-
-
-def choose_payload(self):
-    os.system("clear")
-    print(h.WHITE+"""
-               _     __,..---""-._                 ';-,
-        ,    _/_),-"`             '-.                `\\
-       \|.-"`    -_)                 '.                ||
-       /`   a   ,                      \              .'/
-       '.___,__/                 .-'    \_        _.-'.'
-          |\  \      \         /`        _`""""""`_.-'
-             _/;--._, >        |   --.__/ `""""""`
-           (((-'  __//`'-......-;\      )
-                (((-'       __//  '--. /   Mouse/RAT
-                          (((-'    __//
-                                 (((-'
-"""+h.WHITE+"Public Version: "+h.GREEN_THIN+"mouse-v1.6-"+SAS+h.WHITE+"\nDeveloped by Entynetproject (Ivan Nikolsky)"+h.ENDC)
-    print(h.WHITE+"-"*40)
-    print("")
-    number_option = 1
-    for key in self.payloads:
-        payload = self.payloads[key]
-        self.print_payload(payload,number_option)
-        number_option += 1
-    print ""
-    while 1:
-        try:
-            # choose payload
-            option = raw_input(h.info_general_raw("Choose an payload> "))
-            if not option:
-              continue
-            selected_payload = self.payloads[self.payloads.keys()[int(option) - 1]]
-            # set host and port
-            self.server.set_host_port()
-            # generate payload
-            selected_payload.run(self.server)
-            #run
-            self.prompt_run_server()
-            break
-        except KeyboardInterrupt:
-            break
-        except Exception as e:
-            print e
-            break
-
-
-def menu(self,err=""):
-    while 1:
-         try:
-            h.clear()
-            if err:
-                print err
-            if self.server.debug:
-                print "Debug On"
-            sys.stdout.write(self.banner_text)
-            option = raw_input(self.main_menu_text)
-            choose = {
-                "1" : self.start_single_server,
-                "2" : self.start_multi_handler,
-                "3" : self.choose_payload,
-                "0" : self.exit_menu
-            }
-            try:
-                choose[option]()
-                self.menu()
-            except KeyError:
-                if option:
-                    self.menu()
-                else:
-                    self.menu()
-            except KeyboardInterrupt:
+    def import_payloads(self):
+        path = "modules/payloads"
+        sys.path.append(path)
+        modules = dict()
+        for mod in os.listdir(path):
+            if mod == '__init__.py' or mod[-3:] != '.py':
                 continue
-                # TODO: quit socket listener
+            else:
+                m = __import__(mod[:-3]).payload()
+                modules[m.name] = m
+        return modules
+
+
+    def exit_menu(self):
+        os.system("clear")
+        print(h.WHITE+"""
+               _     __,..---""-._                 ';-,
+        ,    _/_),-"`             '-.                `\\
+       \|.-"`    -_)                 '.                ||
+       /`   a   ,                      \              .'/
+       '.___,__/                 .-'    \_        _.-'.'
+          |\  \      \         /`        _`""""""`_.-'
+             _/;--._, >        |   --.__/ `""""""`
+           (((-'  __//`'-......-;\      )
+                (((-'       __//  '--. /   Mouse/RAT
+                          (((-'    __//
+                                 (((-'
+"""+h.WHITE+"Public Version: "+h.GREEN_THIN+"mouse-v1.6-"+SAS+h.WHITE+"\nDeveloped by Entynetproject (Ivan Nikolsky)"+h.ENDC)
+        print(h.WHITE+"-"*40)
+        print("")
+        exit()
+
+
+    def choose_payload(self):
+        os.system("clear")
+        print(h.WHITE+"""
+               _     __,..---""-._                 ';-,
+        ,    _/_),-"`             '-.                `\\
+       \|.-"`    -_)                 '.                ||
+       /`   a   ,                      \              .'/
+       '.___,__/                 .-'    \_        _.-'.'
+          |\  \      \         /`        _`""""""`_.-'
+             _/;--._, >        |   --.__/ `""""""`
+           (((-'  __//`'-......-;\      )
+                (((-'       __//  '--. /   Mouse/RAT
+                          (((-'    __//
+                                 (((-'
+"""+h.WHITE+"Public Version: "+h.GREEN_THIN+"mouse-v1.6-"+SAS+h.WHITE+"\nDeveloped by Entynetproject (Ivan Nikolsky)"+h.ENDC)
+        print(h.WHITE+"-"*40)
+        print("")
+        number_option = 1
+        for key in self.payloads:
+            payload = self.payloads[key]
+            self.print_payload(payload,number_option)
+            number_option += 1
+        print ""
+        while 1:
+            try:
+                # choose payload
+                option = raw_input(h.info_general_raw("Choose an payload> "))
+                if not option:
+                  continue
+                selected_payload = self.payloads[self.payloads.keys()[int(option) - 1]]
+                # set host and port
+                self.server.set_host_port()
+                # generate payload
+                selected_payload.run(self.server)
+                #run
+                self.prompt_run_server()
+                break
+            except KeyboardInterrupt:
+                break
+            except Exception as e:
+                print e
+                break
+
+
+    def menu(self,err=""):
+        while 1:
+            try:
+                h.clear()
+                if err:
+                    print err
+                if self.server.debug:
+                    print "Debug On"
+                sys.stdout.write(self.banner_text)
+                option = raw_input(self.main_menu_text)
+                choose = {
+                    "1" : self.start_single_server,
+                    "2" : self.start_multi_handler,
+                    "3" : self.choose_payload,
+                    "0" : self.exit_menu
+                }
+                try:
+                    choose[option]()
+                    self.menu()
+                except KeyError:
+                    if option:
+                        self.menu()
+                    else:
+                        self.menu()
+                except KeyboardInterrupt:
+                    continue
+                    # TODO: quit socket listener
             except KeyboardInterrupt:
                 
-                 exit()
+                exit()
 
 
 if __name__ == "__main__":
