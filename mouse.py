c2da9a2103a2198caadba150d37dc9d291adb5fc
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 12:50:19 +0300

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -30,17 +30,14 @@ class mouse:
                           (((-'    __//
                                  (((-'
 """+h.WHITE+"Public Version: "+h.GREEN_THIN+"mouse-v1.6-"+SAS+h.WHITE+"\nDeveloped by Entynetproject (Ivan Nikolsky)\n"+h.ENDC
-        self.main_menu_text = h.WHITE+"-"*40+"""
+        self.main_menu_text = h.WHITE+"-"*40+""""""
 
-    """+h.GREEN_THIN+"""1"""+h.WHITE+""") Start Server
-    """+h.GREEN_THIN+"""2"""+h.WHITE+""") Start MultiHandler
-    """+h.GREEN_THIN+"""3"""+h.WHITE+""") Create Payload
-    """+h.GREEN_THIN+"""0"""+h.WHITE+""") Exit
-
-"""+h.NES
-
-
-
+print(h.GREEN_THIN+"1"+h.WHITE+") Start Server"
+print(h.GREEN_THIN+"2"+h.WHITE+") Start MultiHandler"
+print(h.GREEN_THIN+"3"+h.WHITE+") Create Payload"
+print(h.GREEN_THIN+"0"+h.WHITE+") Exit"
+      print(h.NES)
+      
 
     # Actions
     def print_payload(self,payload,number_option):
