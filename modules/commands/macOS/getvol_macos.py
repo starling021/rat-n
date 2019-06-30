ab853ad10216d0c58b9f52eb0f0446fa7e9f244c
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 17:37:21 +0300

    Update getvol_macos.py

--- modules/commands/macOS/getvol_macos.py
+++ modules/commands/macOS/getvol_macos.py
@@ -2,7 +2,7 @@ import json
 class command:
     def __init__(self):
         self.name = "getvol"
-        self.description = "get speaker output volume"
+        self.description = "Get speaker output volume."
     	self.type = "applescript"
 
     def run(self,session,cmd_data):
