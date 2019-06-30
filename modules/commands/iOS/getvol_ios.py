eb81a3f50e47d1f8467551e715d7bfbe57c742c8
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 18:02:07 +0300

    Update getvol_ios.py

--- modules/commands/iOS/getvol_ios.py
+++ modules/commands/iOS/getvol_ios.py
@@ -1,7 +1,7 @@
 class command:
     def __init__(self):
         self.name = "getvol"
-        self.description = "get volume level"
+        self.description = "Get volume level."
     
     def run(self,session,cmd_data):
         print session.send_command(cmd_data)
