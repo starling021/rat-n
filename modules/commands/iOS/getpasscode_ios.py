6c95c4fc0bbd671aaf77b65d175f02a9f431f105
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 17:59:54 +0300

    Update getpasscode_ios.py

--- modules/commands/iOS/getpasscode_ios.py
+++ modules/commands/iOS/getpasscode_ios.py
@@ -1,7 +1,7 @@
 class command:
     def __init__(self):
         self.name = "getpasscode"
-        self.description = "retreive the device passcode"
+        self.description = "Retreive the device passcode."
     
     def run(self,session,cmd_data):
         error = session.send_command(cmd_data)
