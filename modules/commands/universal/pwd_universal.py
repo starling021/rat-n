02d7e7af5b10109ba406ae1affe0f3b63da67459
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-30 17:31:25 +0300

    Update pwd_universal.py

--- modules/commands/universal/pwd_universal.py
+++ modules/commands/universal/pwd_universal.py
@@ -1,7 +1,7 @@
 class command:
     def __init__(self):
         self.name = "pwd"
-        self.description = "show current directory"
+        self.description = "Show current directory."
     
     def run(self,session,cmd_data):
         print session.send_command(cmd_data)
