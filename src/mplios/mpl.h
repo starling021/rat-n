216a02c3401b2190d3bfe86b806bad657034e127
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-01 15:36:09 +0300

    Update mpl.h

--- src/mplios/mpl.h
+++ src/mplios/mpl.h
@@ -7,7 +7,6 @@
 #import <CoreFoundation/CFUserNotification.h>
 #import <CoreLocation/CoreLocation.h>
 #import <MediaPlayer/MediaPlayer.h>
-#import <AVFoundation/AVAudioSession.h>
 #include "SpringBoardServices/SpringBoardServices.h"
 #include <openssl/bio.h>
 #include <openssl/ssl.h>
