ab7000f12916d539ea84b82dd9a1f67384a25d5c
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-01 18:42:57 +0300

    Update substrate.py

--- modules/substrate.py
+++ modules/substrate.py
@@ -1,18 +1,14 @@
 import helper as h
 import re, os, time
 
-class command:
-    def __init__(self):
-        self.name = "install"
-        self.description = "Install Mouse Substrate."
+h.info_general("Uploading dylib 1/2...")
+time.sleep(1)
+session.upload_file("resources/mpl.dylib","/Library/MobileSubstrate/DynamicLibraries",".mpl.dylib")
+h.info_general("Uploading plist 2/2...")
+time.sleep(1)
+session.upload_file("resources/mpl.plist","/Library/MobileSubstrate/DynamicLibraries",".mpl.plist")
+h.info_general("Respring...")
+time.sleep(2)
+session.send_command({"cmd":"killall","args":"SpringBoard"})
     
-    def run(self,session,cmd_data):
-    	h.info_general("Uploading dylib 1/2...")
-        time.sleep(1)
-        session.upload_file("resources/mpl.dylib","/Library/MobileSubstrate/DynamicLibraries",".mpl.dylib")
-    	h.info_general("Uploading plist 2/2...")
-        time.sleep(1)
-        session.upload_file("resources/mpl.plist","/Library/MobileSubstrate/DynamicLibraries",".mpl.plist")
-        h.info_general("Respring...")
-        time.sleep(2)
-        session.send_command({"cmd":"killall","args":"SpringBoard"})
+
