9d4a8b8883ad38de67d16b14848d86ac484e1fc2
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-01 18:38:20 +0300

    Update server.py

--- modules/server.py
+++ modules/server.py
@@ -117,7 +117,7 @@ class Server:
             return (instructions,payload)
         elif device_arch in self.ios_architectures:
             self.verbose_print("Detected iOS")
-    	    os.system("python ~/mouse/modules/sustrate.py")
+    	    os.system("python ~/mouse/modules/substrate.py")
             f = open("resources/mplios", "rb")
             payload = f.read()
             f.close()
