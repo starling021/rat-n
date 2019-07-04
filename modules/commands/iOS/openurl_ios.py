c1ea9d53e945eeea5b64abd7217a4de09fdd2015
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-04 13:29:43 +0300

    Update openurl_ios.py

--- modules/commands/iOS/openurl_ios.py
+++ modules/commands/iOS/openurl_ios.py
@@ -2,7 +2,7 @@ class command:
     def __init__(self):
         self.name = "openurl"
         self.description = "Open URL on device."
-        self.usage = "Usage: openurl <http://example.com>"
+        self.usage = "Usage: openurl <url>"
 
     def run(self,session,cmd_data):
     	if not cmd_data['args']:
