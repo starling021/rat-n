230a1788f861f0c15917b755bc6e532ab3054cc5
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-04 19:03:47 +0300

    Update alert_ios.py

--- modules/commands/iOS/alert_ios.py
+++ modules/commands/iOS/alert_ios.py
@@ -8,7 +8,7 @@ class command:
         self.type = "native"
 
     def run(self,session,cmd_data):
-        title = raw_input("Title: ")
-        message = raw_input("Message: ")
+        title = raw_input(h.info_general+"Alert Title: ")
+        message = raw_input(h.info_general+"Alert Message: ")
         session.send_command({"cmd":"alert","args":json.dumps({"title":title,"message":message})})
         return ""
