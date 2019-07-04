a3e281fa614d1a7d8cd63a88a5f2f5c3e0e199a1
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-04 13:44:41 +0300

    Update brightness_macos.py

--- modules/commands/macOS/brightness_macos.py
+++ modules/commands/macOS/brightness_macos.py
@@ -2,7 +2,7 @@ class command:
     def __init__(self):
         self.name = "brightness"
         self.description = "Adjust screen brightness."
-        self.usage = "Usage: brightness <level: 0-100>"
+        self.usage = "Usage: brightness <level: 0-1>"
         self.type = "native"
     
     def run(self,session,cmd_data):
