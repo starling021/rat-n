2bcf6ded4b541c1a3c3eb99f13bd19b5989c63dd
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-08 12:38:34 +0200

    Update mpl.py

--- resources/mpl.py
+++ resources/mpl.py
@@ -1,4 +1,33 @@
 #!/usr/bin/python
+
+# 
+#            --------------------------------------------------
+#                           Mouse Payload Loader                
+#            --------------------------------------------------
+#          Copyright (C) <2015>  <Entynetproject (Ivan Nikolsky)>
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    About Author :   
+#    Founder   : Entynetproject (Ivan Nikolsky)
+#    Site      : http://entynetproject.simplesite.com/
+#    Instagram : @entynetproject 
+#    Twitter   : @entynetproject
+#    Email     : entynetproject@gmail.com
+#
+
 from uuid import getnode as get_mac
 import json, os, base64, sys, socket, ssl, getpass, subprocess
 from os.path import expanduser
