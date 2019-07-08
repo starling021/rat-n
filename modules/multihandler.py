20a2280539015580a34a10589def2613ba661f8d
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-08 12:37:19 +0200

    Update multihandler.py

--- modules/multihandler.py
+++ modules/multihandler.py
@@ -1,3 +1,31 @@
+# 
+#            --------------------------------------------------
+#                           Mouse Payload Loader                
+#            --------------------------------------------------
+#          Copyright (C) <2015>  <Entynetproject (Ivan Nikolsky)>
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    About Author :   
+#    Founder   : Entynetproject (Ivan Nikolsky)
+#    Site      : http://entynetproject.simplesite.com/
+#    Instagram : @entynetproject 
+#    Twitter   : @entynetproject
+#    Email     : entynetproject@gmail.com
+#
+
 from modules import helper as h
 import threading, socket, time, sys
 
