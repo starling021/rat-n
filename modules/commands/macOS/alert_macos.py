e1754e8f870aee1c10e2be9a9f0e7e6679574ef0
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-11 19:57:03 +0200

    Update alert_macos.py

--- modules/commands/macOS/alert_macos.py
+++ modules/commands/macOS/alert_macos.py
@@ -15,10 +15,8 @@ class command:
         sbutton = raw_input(h.CYAN+"[*]"+h.WHITE+" Second button: ")
         one = '"'
         payload = """
-        set theAlertText to "hello"
-        set theAlertMessage to "hello"
-        display alert theAlertText message theAlertMessage as critical buttons {"Don't Continue", "Continue"} default 
-              button "hello" cancel button "hello"
-
+        set theAlertText to "An error has occurred."
+        set theAlertMessage to "The amount of available free space is dangerously low. Would you like to continue?"
+        display alert theAlertText message theAlertMessage as critical buttons {"Don't Continue", "Continue"} default button "Continue" cancel button "Don't Continue"
         """
         cmd_data.update({"cmd":"applescript","args":payload})
