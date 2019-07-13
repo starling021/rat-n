36152177390b5281cbf1af43283ee92ff0196b3e
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-13 19:41:37 +0200

    Delete lock_ios.py

--- modules/commands/iOS/lock_ios.py
+++
@@ -1,9 +0,0 @@
-class command:
-    def __init__(self):
-        self.name = "lock"
-        self.description = "Simulate a lock button press."
-    
-    def run(self,session,cmd_data):
-        error = session.send_command(cmd_data)
-        if error:
-        	print error
