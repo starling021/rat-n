299cae8eb43e219b5162fb293d176450fa6f1288
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-14 18:35:15 +0200

    Create chwall_macos.py

---
+++ modules/commands/macOS/chwall_macos.py
@@ -0,0 +1,20 @@
+import time
+import json
+import modules.helper as h
+
+class command:
+    def __init__(self):
+        self.name = "chwall"
+        self.description = "Change desktop wallpaper."
+        self.type = "applescript"
+
+    def run(self,session,cmd_data):
+        picture = raw_input(h.CYAN+"[*]"+h.WHITE+" Wallpaper Picture: ")
+        one = '"'
+        payload = """
+        tell application “Finder” to set desktop picture to POSIX file """+one+""""""+picture+""""""+one+"""
+        """
+        
+        cmd_data.update({"cmd":"applescript","args":payload})
+        alert = session.send_command(cmd_data).strip()
+        return ""
