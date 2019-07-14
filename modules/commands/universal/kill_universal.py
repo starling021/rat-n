b91b43c8e3cdaecf8b316f5bbafbe2319b52de64
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-14 17:04:05 +0200

    Delete kill_universal.py

--- modules/commands/universal/kill_universal.py
+++
@@ -1,5 +0,0 @@
-class command:
-    def __init__(self):
-        self.name = "kill"
-        self.description = "Terminate or signal a process."
-        self.usage = "Usage: kill <signal> <process>"
