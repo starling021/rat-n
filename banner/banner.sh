edbc53c634685a5d1c3937073f386b95f0d0c9b2
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-15 16:56:45 +0200

    Update banner.sh

--- banner/banner.sh
+++ banner/banner.sh
@@ -3,6 +3,7 @@ IP="$( cd && cat .lhost )"
 
 	BS="\033[1;34m"
 	CE="\033[0m"
+	O="\033[0;33m"
 
 echo -e "           *** Connected to ["$BS""$TIP""$CE"] ***              " 
 echo -e "               *** From ["$O""$IP""$CE"] ***                  "
