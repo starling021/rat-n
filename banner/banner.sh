517a36130d9f9b32843b834f57e9726d8228db89
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-15 16:55:46 +0200

    Update banner.sh

--- banner/banner.sh
+++ banner/banner.sh
@@ -6,7 +6,7 @@ IP="$( cd && cat .lhost )"
 
 echo -e "           *** Connected to ["$BS""$TIP""$CE"] ***              " 
 echo -e "               *** From ["$O""$IP""$CE"] ***                  "
-echo -e "                  *** On port ["$O""$PORT""$CE] ***                     "
+echo -e "                  *** On port ["$O""$PORT""$CE"] ***                     "
 cd 
 cd mouse
 trap '2'
