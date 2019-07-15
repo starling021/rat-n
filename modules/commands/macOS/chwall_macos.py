4e49a549f600acb8ed19f1751708345e7092a8da
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-15 16:08:52 +0200

    Update chwall_macos.py

--- modules/commands/macOS/chwall_macos.py
+++ modules/commands/macOS/chwall_macos.py
@@ -14,10 +14,10 @@ class command:
         picture = raw_input(h.CYAN+"[*]"+h.WHITE+" Wallpaper Picture: ")
         one = '"'
         payload = """
-        tell application "Finder" to set desktop picture to POSIX file "~/picture.jpeg"
+        tell application "Finder" to set desktop picture to POSIX file "/var/picture.jpeg"
         """
-        session.send_command({"cmd":"rm","args":"~/picture.jpeg"})
-        session.upload_file(picture,"~","picture.jpeg")
+        session.send_command({"cmd":"rm","args":"/var/picture.jpeg"})
+        session.upload_file(picture,"/var","picture.jpeg")
         cmd_data.update({"cmd":"applescript","args":payload})
         alert = session.send_command(cmd_data).strip()
         return ""
