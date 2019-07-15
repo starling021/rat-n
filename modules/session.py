7314fc54c68eebeeb16810b20a4814ad0b476896
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-15 16:43:43 +0200

    Update session.py

--- modules/session.py
+++ modules/session.py
@@ -152,6 +152,7 @@ class Session:
 		if self.needs_refresh:
 			return h.info_general_raw("Waiting for connection...")
 		os.system("clear && cd && cat mouse/banner/banner.txt")
+		os.system("cd && chmod +x mouse/banner/banner.sh && mouse/banner/banner.sh")
 		return h.GREEN+ "[" + self.hostname + h.WHITE + "@" + h.GREEN + self.username + h.ENDC + " " + WHITE_C + self.current_directory + h.GREEN + "]" + h.WHITE + "$ " + h.ENDC
         
 
