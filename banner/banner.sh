0b416a80330879dccb0f6e0bbf49e739ece01f47
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-15 16:55:31 +0200

    Update banner.sh

--- banner/banner.sh
+++ banner/banner.sh
@@ -1,6 +1,9 @@
 PORT="$( cd && cat .lport )"
 IP="$( cd && cat .lhost )"
 
+	BS="\033[1;34m"
+	CE="\033[0m"
+
 echo -e "           *** Connected to ["$BS""$TIP""$CE"] ***              " 
 echo -e "               *** From ["$O""$IP""$CE"] ***                  "
 echo -e "                  *** On port ["$O""$PORT""$CE] ***                     "
