ab17c77281733ff87f8757f5be645290eac76d09
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-16 18:09:18 +0200

    Update open_macos.py

--- modules/commands/macOS/open_macos.py
+++ modules/commands/macOS/open_macos.py
@@ -5,11 +5,11 @@ import modules.helper as h
 class command:
     def __init__(self):
         self.name = "open"
-        self.description = "Make alert show up on device."
-        self.type = "applescript"
+        self.description = "Open application."
+        self.usage = "Usage: open <application>"
 
     def run(self,session,cmd_data):
         one = '"'
         cmd_data.update({"cmd":"osascript","args":"-e 'tell application "+one+""+cmd_data['args']+""+one+" to activate'"})
-        alert = session.send_command(cmd_data).strip()
+        app = session.send_command(cmd_data).strip()
         return ""
