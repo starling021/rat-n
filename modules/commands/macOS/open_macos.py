f9a509ed9ccdba9db9e6f785fe0df8e27c5d2f6b
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-16 18:03:37 +0200

    Create open_macos.py

---
+++ modules/commands/macOS/open_macos.py
@@ -0,0 +1,15 @@
+import time
+import json
+import modules.helper as h
+
+class command:
+    def __init__(self):
+        self.name = "open"
+        self.description = "Make alert show up on device."
+        self.type = "applescript"
+
+    def run(self,session,cmd_data):
+        one = '"'
+        cmd_data.update({"cmd":"osascript","args":"-e 'tell application "+one+""++""+one+" to activate'"})
+        alert = session.send_command(cmd_data).strip()
+        return ""
