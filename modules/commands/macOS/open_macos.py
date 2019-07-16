69072afda05cb757e123ec0148823ee7a6dcaaa3
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-16 18:27:38 +0200

    Update open_macos.py

--- modules/commands/macOS/open_macos.py
+++ modules/commands/macOS/open_macos.py
@@ -10,6 +10,8 @@ class command:
 
     def run(self,session,cmd_data):
         one = '"'
+        if cmd_data['args'] == "":
+            print("Usage: close <application>")
         cmd_data.update({"cmd":"osascript","args":"-e 'tell application "+one+""+cmd_data['args']+""+one+" to activate'"})
         app = session.send_command(cmd_data).strip()
         return ""
