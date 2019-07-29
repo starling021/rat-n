625a1d3fc550963cb561ed58dbbc4c01c21589b1
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-29 08:53:14 +0200

    Update bundleids_ios.py

--- modules/commands/iOS/bundleids_ios.py
+++ modules/commands/iOS/bundleids_ios.py
@@ -1,7 +1,10 @@
+import helper as h
+
 class command:
     def __init__(self):
         self.name = "bundleids"
         self.description = "List bundle identifiers."
     
     def run(self,session,cmd_data):
+        print h.WHITEBU+"Bundle Identifiers:"+h.ENDC
         print session.send_command(cmd_data).rstrip()
