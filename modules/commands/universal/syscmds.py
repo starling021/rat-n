3e3bd7a51a396eeb3e7af38f21e1bb7bf9d4c0db
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-29 19:21:09 +0200

    Create syscmds.py

---
+++ modules/commands/universal/syscmds.py
@@ -0,0 +1,13 @@
+import time
+import json
+import os
+import modules.helper as h
+
+class command:
+    def __init__(self):
+        self.name = "syscmds"
+        self.description = "Display all system commands."
+
+    def run(self,session,cmd_data):
+        print h.WHITEBU+"System Commands:"+h.ENDC
+        os.system("compgen -c | sort")
