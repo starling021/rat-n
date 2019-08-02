76b6dadc47cd3e8ebecd2cba9e9a311afc26f3a7
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-08-02 12:21:34 +0200

    Update syscmds_universal.py

--- modules/commands/universal/syscmds_universal.py
+++ modules/commands/universal/syscmds_universal.py
@@ -1,3 +1,9 @@
+class command:
+    def __init__(self):
+        self.name = "syscmds"
+        self.description = "Show all system commands."
+    
+    def run(self,session,cmd_data):
         payload = "-c"
         cmd_data.update({"cmd":"compgen","args":payload})
-        print(session.send_command(cmd_data))
+        print session.send_command(cmd_data)
