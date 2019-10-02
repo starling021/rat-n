27edb9f06c96632d7cc25a46edbbc25e414bd206
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-03 01:05:32 +0300

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -35,7 +35,7 @@ import platform
 import time
 
 os.system("chmod +x bin/mouse && bin/mouse --check-directory")
-os.chdir(os.path.expanduser(~/mouse))
+os.chdir(os.path.expanduser("~/mouse"))
 
 SAS='dev'
 
