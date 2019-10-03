974af1fdcc28643ebcada111f88058ddb5c7f9a8
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-03 03:41:48 +0300

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -30,7 +30,7 @@
 
 import os
 
-print '\033]2;Mouse Payload Loader\a'
+print "\033]2;Mouse Payload Loader\a"
 os.system("chmod +x bin/mouse && bin/mouse --check-directory")
 os.chdir(os.path.expanduser("~/mouse"))
 
