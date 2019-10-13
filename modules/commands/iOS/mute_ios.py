b170669637012d62606d9a8e0356223bc1bc195b
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-13 23:19:39 +0300

    Update mute_ios.py

--- modules/commands/iOS/mute_ios.py
+++ modules/commands/iOS/mute_ios.py
@@ -18,4 +18,4 @@ class command:
             cmd_data = {'cmd':'mute','args':''}
         error = session.send_command(cmd_data)
         if error:
-        	print(h.RED+"[-]"+h.WHITE+" MPL Substrate is not installed!")
+        	h.info_error("Unexpected error!")
