aec4c12139663e5da6788bf1dbf1b5d9a0e1bd90
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-13 21:54:22 +0300

    Update msub_ios.py

--- modules/commands/iOS/msub_ios.py
+++ modules/commands/iOS/msub_ios.py
@@ -5,8 +5,11 @@ class command:
     def __init__(self):
         self.name = "msub"
         self.description = "Install MPL Substrate."
+        self.usage = "Usage: msub [install|uninstall]"
     
     def run(self,session,cmd_data):
+        if not cmd_data['args']:
+    		print self.usage
     	h.info_general("Uploading dylib 1/2...")
         session.upload_file("substrate/mpl.dylib","/Library/MobileSubstrate/DynamicLibraries",".mpl.dylib")
     	h.info_general("Uploading plist 2/2...")
