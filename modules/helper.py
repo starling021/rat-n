af355b5a5baaf17c1bfc972fd2211ff10a37eceb
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-10 00:40:09 +0300

    Update helper.py

--- modules/helper.py
+++ modules/helper.py
@@ -34,7 +34,7 @@ import socket
 WINDOWS = sys.platform.startswith('win')
 #colors
 GREEN = '' if WINDOWS else '\033[0;33m'
-RED = '' if WINDOWS else '\033[31m'
+RED = '' if WINDOWS else '\033[1;31m'
 WHITE = '' if WINDOWS else '\033[0;97m'
 GREEN_THIN = '' if WINDOWS else '\033[0;33m'
 CYAN = '' if WINDOWS else '\033[1;34m'
