52c6bae15e622486dde2cb4a291b0bb7c5fe99dd
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-10 00:19:27 +0300

    Update build.sh

--- substrate/mpl/build.sh
+++ substrate/mpl/build.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
- #            --------------------------------------------------
- #                              Mouse Substrate               
- #            --------------------------------------------------
- #                  Copyright (C) <2019>  <Entynetproject>
- #
- #        This program is free software: you can redistribute it and/or modify
- #        it under the terms of the GNU General Public License as published by
- #        the Free Software Foundation, either version 3 of the License, or
- #        any later version.
- #
- #        This program is distributed in the hope that it will be useful,
- #        but WITHOUT ANY WARRANTY; without even the implied warranty of
- #        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- #        GNU General Public License for more details.
- #
- #        You should have received a copy of the GNU General Public License
- #        along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
- if [[ "$1" = "-b" || "$1" = "--build" ]]
- then
-
+#            --------------------------------------------------
+#                              Mouse Substrate               
+#            --------------------------------------------------
+#                  Copyright (C) <2019>  <Entynetproject>
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
          rm -rf .cydia-package
  	 # directory stucture
@@ -40,4 +36,3 @@
          echo "#!/bin/bash" >> .cydia-package/DEBIAN/postinst
          chmod +x .cydia-package/DEBIAN/postinst
          dpkg -b .cydia-package msub.deb
- 	 fi
