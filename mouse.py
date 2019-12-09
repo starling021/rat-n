b7d0a7af686d7131da255f9c102389c0dd449aa6
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-09 21:11:48 +0300

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -3,7 +3,7 @@
 #            --------------------------------------------------
 #                           Mouse Payload Loader                
 #            --------------------------------------------------
-#                  Copyright (C) <2015>  <Entynetproject>
+#                  Copyright (C) <2019>  <Entynetproject>
 #
 #        This program is free software: you can redistribute it and/or modify
 #        it under the terms of the GNU General Public License as published by
