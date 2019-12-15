72ce2a72c730ef0605779fe9592c4027eb4ca671
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-15 19:53:37 +0300

    Update update.sh

--- etc/update.sh
+++ etc/update.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#blue start 
+	BS="\033[1;34m"
+#color end
+	CE="\033[0m"
+#red start
+	RS="\033[1;31m"
+#green start
+	GNS="-e \033[1;32m"
+#white start
+  WHS="\033[0;97m"
+        
 if [[ -d /data/data/com.termux ]]
 then
 if [[ -f /data/data/com.termux/files/usr/bin/mouse ]]
