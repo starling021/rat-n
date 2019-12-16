6109f089594e16e280b3ddff4c118b012b634917
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-16 03:04:45 +0300

    Create upload_universal.py

---
+++ etc/commands/upload_universal.py
@@ -0,0 +1,42 @@
+import modules.helper as h
+import re, os
+
+class command:
+    def __init__(self):
+        self.name = "upload"
+        self.description = "Upload local file."
+        self.usage = "Usage: upload <path_to_file> <remote_path>"
+    
+    def run(self,session,cmd_data):
+        if not cmd_data['args']:
+            print self.usage
+            return
+        else:
+            paths = re.split(r'(?<!\\) ', cmd_data['args'].rstrip())
+            if len(paths) > 2:
+                print "Usage: upload <path_to_file> <remote_path>"
+                return
+            
+            local_dir = os.path.split(paths[0])[0]
+            local_file = os.path.split(paths[0])[1]
+            
+            if len(paths) == 1:
+                remote_dir = "."
+                remote_file = local_file
+            else:
+                remote_dir = os.path.split(paths[1])[0]
+                if not remote_dir:
+                    remote_dir = "."
+                remote_file = os.path.split(paths[1])[1]
+                if not remote_file:
+                    remote_file = local_file
+
+            w = os.environ['OLDPWD']
+            os.chdir(w)
+
+            session.upload_file(paths[0],remote_dir,remote_file)
+
+            g = os.environ['HOME']
+            os.chdir(g + "/mouse")
+
+            h.info_general("File successfully uploaded!")
