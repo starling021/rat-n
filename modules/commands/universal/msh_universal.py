108508e63b722b72fc561382ede4b4165aa49387
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-29 22:29:14 +0100

    Update msh_universal.py

--- modules/commands/universal/msh_universal.py
+++ modules/commands/universal/msh_universal.py
@@ -1,7 +1,15 @@
+import modules.helper as h
+
 class command:
     def __init__(self):
         self.name = "msh"
         self.description = "Mouse (MPL) Shell."
     
     def run(self,session,cmd_data):
-        
+        while 1:
+			#prepare command
+			msh = raw_input("msh# ")
+            if msh == "exit":
+                return
+            else:
+                h.info_error("Invalid command: "+msh)
