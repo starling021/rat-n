5953ab5df3b2a2b3e470610b386bf1966cc09d04
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-30 21:11:53 +0100

    Update session.py

--- modules/session.py
+++ modules/session.py
@@ -120,7 +120,7 @@ class Session:
 		    return h.info_general_raw("Waiting for connection...")
 		os.system("printf '\033]2;Mouse CLI\a'")
 		mousel = "\033[4;77m"
-		return h.WHITE+"mouse("+h.GREEN+self.hostname+h.WHITE+"@"+h.GREEN+self.username+h.WHITE+")> "
+		return h.WHITE+"("+h.GREEN+self.hostname+h.WHITE+"@"+h.GREEN+self.username+h.WHITE+")> "
 
 	def tab_complete(self, text, state):
 		# TODO: tab complete 'ls ', use get_completer_delims
