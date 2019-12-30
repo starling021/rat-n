9f2bd90d403fe4466cf14ea4b3f33fcf2a8846f4
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-30 14:33:19 +0100

    Update session.py

--- modules/session.py
+++ modules/session.py
@@ -199,10 +199,6 @@ class Session:
                 print("==============")
 		os.system("cat resources/local_cmds.txt")
 		
-		print("\nSystem Commands")
-		print("===============")
-		os.system("cat resources/system_cmds.txt")
-		
 		print("\nSettings Commands")
                 print("=================")
 		
