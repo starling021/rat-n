2228b7f6ab33c31a2ab165dfbc93a02a515ecbc4
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-30 17:30:44 +0100

    Update session.py

--- modules/session.py
+++ modules/session.py
@@ -117,10 +117,10 @@ class Session:
 	def get_handle(self):
 		"""Interact with an active session"""
 		if self.needs_refresh:
-			return h.info_general_raw("Waiting for connection...")
+		    return h.info_general_raw("Waiting for connection...")
 		os.system("printf '\033]2;Mouse CLI\a'")
 		mousel = "\033[4;77m"
-		return mousel+"mouse"+h.ENDC+"("+str(self.conn.getpeername()[0]+")> "
+		return mousel+"mouse"+h.ENDC+"()> "
 
 	def tab_complete(self, text, state):
 		# TODO: tab complete 'ls ', use get_completer_delims
