0af6a46f7e0e80acd8d0f2886f65836e41b03ab2
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-30 13:24:54 +0100

    Update msh_universal.py

--- modules/commands/universal/msh_universal.py
+++ modules/commands/universal/msh_universal.py
@@ -4,19 +4,26 @@ class command:
     def __init__(self):
         self.name = "msh"
         self.description = "Mouse (MPL) Shell."
+	
+    def msh(self):
+	os.system("printf '\033]2;Mouse CLI\a'")
+        return h.GREEN+ "[" + self.hostname + h.WHITE + "@" + h.GREEN + self.username + h.ENDC + " " + WHITE_C + self.current_directory + h.GREEN + "]" + h.WHITE + "$ " + h.ENDC
     
     def run(self,session,cmd_data):
         while 1:
 	    #prepare command
-	    msh = raw_input("msh# ")
-            if msh == "exit":
+	    msh = raw_input(self.msh())
+		
+	    if not raw or raw.replace(" ","") == "":
+	        continue
+	    mshd = raw.split()[0]
+	    mshd_data = {"cmd": cmd, "args":msh[len(mshd) + 1:]}
+	    if mshd == "exit":
                 return
             else:
-		if not msh['args']:
-		    argvs = ""
-		else:
-		    argvs = msh['args']
-		msh_command = {"cmd": msh, "args":argvs}
-		result = session.send_command(msh_command)
-		if result:
-		    print(result)
+		try:
+		    result = session.send_command(mshd_data)
+		    if result:
+			print result.rstrip()
+		    except KeyboardInterrupt:
+			session.send_command({"cmd":"killtask"})
