2cb27db077da2919b64f31d1cd4dcd8c4721b238
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-30 16:36:12 +0100

    Delete pwd_universal.py

--- modules/commands/universal/pwd_universal.py
+++
@@ -1,7 +0,0 @@
-class command:
-    def __init__(self):
-        self.name = "pwd"
-        self.description = "Show current directory."
-    
-    def run(self,session,cmd_data):
-        print session.send_command(cmd_data)
