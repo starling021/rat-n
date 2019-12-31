dec62504d9a8001c8e179bd847c7e9d56a2c632b
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-31 18:16:07 +0100

    Update exec.py

--- etc/modules/commands/local/exec.py
+++ etc/modules/commands/local/exec.py
@@ -1,7 +1,22 @@
-w = os.environ['OLDPWD']
+import os
+from os.path import expanduser
+import modules.helper as h
+
+class command:
+    def __init__(self):
+        self.name = "exec"
+        self.description = "Execute local shell commands."
+    
+    def run(self,session,cmd_data):
+        if not cmd_data['args']:
+            print "Usage: exec <command>"
+            return
+        else:
+            print(h.CYAN+"[*]"+h.WHITE+" exec: "+cmd_data['args'])
+            print("")
+            w = os.environ['OLDPWD']
             os.chdir(w)
-
-            session.upload_file(paths[0],remote_dir,remote_file)
-
+            split_args = cmd_data['args'].split()
+            os.system(cmd_data['args'])
             g = os.environ['HOME']
-            os.chdir(g + "/mouse")
+            os.chdir(g + "/mouse")      
