d25d37b41422d754610ab7a9344cf247aebcdc54
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-01-01 15:42:47 +0100

    Update multihandler.py

--- modules/multihandler.py
+++ modules/multihandler.py
@@ -35,7 +35,12 @@ class MultiHandler:
 					if session.uid in self.sessions_uid.keys():
 						if self.sessions_uid[session.uid].needs_refresh:
 							self.update_session(self.sessions_uid[session.uid],session)
-						continue
+						self.sessions_uid[session.uid] = session
+						self.sessions_id[id_number] = session
+						session.id = id_number
+						id_number += 1
+						sys.stdout.write("\n{0} Session {1} opened.{2}\n{3}".format(DOJ,str(session.id),h.WHITE,self.handle))
+						sys.stdout.flush()
 					else:
 						self.sessions_uid[session.uid] = session
 						self.sessions_id[id_number] = session
