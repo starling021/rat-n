e4c2f1e272335e4b02c0bfc157ddf736bcacc2c1
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-01-01 15:46:40 +0100

    Update multihandler.py

--- modules/multihandler.py
+++ modules/multihandler.py
@@ -33,8 +33,8 @@ class MultiHandler:
 				session = self.server.listen_for_stager()
 				if session:
 					if session.uid in self.sessions_uid.keys():
-						self.update_session(self.sessions_uid[session.uid],session)
-						self.sessions_uid[session.uid] = session
+						uid = session.uid + "u"
+						self.sessions_uid[uid] = session
 						self.sessions_id[id_number] = session
 						session.id = id_number
 						id_number += 1
