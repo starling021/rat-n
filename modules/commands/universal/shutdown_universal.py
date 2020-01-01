e6070cc0b7bb17738af5fc0b573aeb2573fdf969
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-01-01 20:51:50 +0100

    Delete shutdown_universal.py

--- modules/commands/universal/shutdown_universal.py
+++
@@ -1,7 +0,0 @@
-class command:
-	def __init__(self):
-		self.name = "shutdown"
-		self.description = "Shutdown device."
-
-	def run(self,session,cmd_data):
-		session.send_command({"cmd":"shutdown","args":""})
