9e25c292aeaae94f51f5b1aba94406c94819054a
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-01-01 21:36:05 +0100

    Delete ldrestart_ios.py

--- modules/commands/iOS/ldrestart_ios.py
+++
@@ -1,7 +0,0 @@
-class command:
-	def __init__(self):
-		self.name = "ldrestart"
-		self.description = "Restart the kernel."
-
-	def run(self,session,cmd_data):
-		session.send_command({"cmd":"ldrestart","args":""})
