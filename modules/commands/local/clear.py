194c3fe5a0e3a06318563bceb67107c099571857
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-01-09 20:37:20 +0300

    Update clear.py

--- modules/commands/local/clear.py
+++ modules/commands/local/clear.py
@@ -3,7 +3,7 @@ import os
 class command:
     def __init__(self):
         self.name = "clear"
-        self.description = "Clean terminal input/output."
+        self.description = "Clean terminal window."
     
     def run(self,session,cmd_data):
         os.system('clear')
