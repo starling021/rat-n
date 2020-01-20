2f311ce2d83a5b737c421e0645460bf5e6fa7472
enty8080 <enty8080@gmail.com>
2020-01-20 21:26:15 +0100

    Add files via upload

--- core/commands/iOS/sysinfo_ios.py
+++
@@ -1,7 +0,0 @@
-class command:
-	def __init__(self):
-		self.name = "sysinfo"
-		self.description = "Show system information."
-    
-	def run(self,session,cmd_data):
-		print session.send_command(cmd_data)
