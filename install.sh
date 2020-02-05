a87826ac93f84091a43662388be669abf321573f
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-06 01:08:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#            --------------------------------------------------
-#                              Mouse Framework                               
-#            --------------------------------------------------
-#                  Copyright (C) <2019>  <Entynetproject>
+#            ---------------------------------------------------
+#                              Mouse Framework                                 
+#            ---------------------------------------------------
+#                Copyright (C) <2019-2020>  <Entynetproject>
 #
 #        This program is free software: you can redistribute it and/or modify
 #        it under the terms of the GNU General Public License as published by
