716de57208a2a004b77d3561189a1b6185444d26
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-15 13:43:06 +0300

    Update rubber_duck_payload.py

--- core/payloads/rubber_duck_payload.py
+++ core/payloads/rubber_duck_payload.py
@@ -23,7 +23,7 @@ import os, time
 
 class payload:
 	def __init__(self):
-		self.name = "Rubber Duck (USB injection)"
+		self.name = "Rubber Duck Payload"
 		self.description = "Arduino payload that replicates keystrokes for shell script execution."
 		self.usage = "Install via ducktoolkit.com site."
 
