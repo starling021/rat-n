2efd5fb3b4a69fc3b9389da82d715f0e0788572a
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-31 12:17:08 +0300

    Update respring_ios.py

--- core/commands/ios/respring_ios.py
+++ core/commands/ios/respring_ios.py
@@ -27,5 +27,6 @@ class command:
 		self.description = "Restart SpringBoard."
 
 	def run(self,session,cmd_data):
+		h.info_general("Restarting SpringBoard...")
 		time.sleep(1)
 		session.send_command({"cmd":"killall","args":"SpringBoard"})
