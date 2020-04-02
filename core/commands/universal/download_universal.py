29b41cfca51e80e6abce70025dd2f67d662b06cb
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-02 23:02:29 +0300

    Update download_universal.py

--- core/commands/universal/download_universal.py
+++ core/commands/universal/download_universal.py
@@ -31,27 +31,27 @@ class command:
 
 	def run(self,session,cmd_data):
 		if not cmd_data['args']:
-            print self.usage
-            return
-        else:
-            paths = re.split(r'(?<!\\) ', cmd_data['args'].rstrip())
-            if len(paths) > 2:
-                print "Usage: download <remote_path> <local_path>"
-                return
+            		print self.usage
+            		return
+        	else:
+            		paths = re.split(r'(?<!\\) ', cmd_data['args'].rstrip())
+            	if len(paths) > 2:
+                	print "Usage: download <remote_path> <local_path>"
+                	return
             
-            local_dir = os.path.split(paths[0])[0]
-            local_file = os.path.split(paths[0])[1]
+            	local_dir = os.path.split(paths[0])[0]
+            	local_file = os.path.split(paths[0])[1]
             
-            if len(paths) == 1:
-                remote_dir = "."
-                remote_file = local_file
-            else:
-                remote_dir = os.path.split(paths[1])[0]
-                if not remote_dir:
-                    remote_dir = "."
-                remote_file = os.path.split(paths[1])[1]
-                if not remote_file:
-                    remote_file = local_file
+            	if len(paths) == 1:
+                	remote_dir = "."
+                	remote_file = local_file
+            	else:
+                	remote_dir = os.path.split(paths[1])[0]
+                	if not remote_dir:
+                    		remote_dir = "."
+                	remote_file = os.path.split(paths[1])[1]
+                	if not remote_file:
+                    		remote_file = local_file
 		
 		h.info_general("Downloading {0}...".format(file_name))
 		data = session.download_file(cmd_data['args'])
