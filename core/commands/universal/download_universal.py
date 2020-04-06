5623f30e7789487dc70b7a45aebad15d7004b569
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-06 22:54:53 +0300

    Update download_universal.py

--- core/commands/universal/download_universal.py
+++ core/commands/universal/download_universal.py
@@ -62,16 +62,17 @@ class command:
 			    rp = os.path.split(cmd_data['args'].split()[1])[0]
                             if os.path.exists(rp):
 			        if os.path.isdir(rp):
+				    prr = os.path.split(cmd_data['args'].split()[1])[0]
 				    rp = os.path.split(cmd_data['args'].split()[1])[1]
 				    pr = os.path.split(cmd_data['args'].split()[0])[1]
 			    	    data = session.download_file(cmd_data['args'].split()[0])
 			    	    h.info_general("Downloading {0}...".format(pr))
 			    	    if data:
-			                h.info_general("Saving to {0}...".format(cmd_data['args'].split()[0]))
-			                f = open(os.path.join(cmd_data['args'].split()[1],rp),'w')
+			                h.info_general("Saving to {0}...".format(cmd_data['args'].split()[1]))
+			                f = open(os.path.join(prr,rp),'w')
 			                f.write(data)
 			                f.close()
-			                h.info_success("Saved to "+cmd_data['args'].split()[1]+"/"+rp+"!")
+			                h.info_success("Saved to "+prr+"/"+rp+"!")
                                 else:
 				    h.info_error("Error: "+rp+": not a directory!")
                    	    else:
