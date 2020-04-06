bdef2081971255458bc33ae81c9efd6970301f62
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-06 10:26:41 +0300

    Update upload_universal.py

--- etc/modules/commands/universal/upload_universal.py
+++ etc/modules/commands/universal/upload_universal.py
@@ -25,7 +25,7 @@ class command:
     def __init__(self):
         self.name = "upload"
         self.description = "Upload local file."
-        self.usage = "Usage: upload <local_file> <remote_dir>"
+        self.usage = "Usage: upload <local_path> <remote_path>"
     
     def run(self,session,cmd_data):
         if not cmd_data['args']:
@@ -33,30 +33,51 @@ class command:
             return
         else:
             paths = re.split(r'(?<!\\) ', cmd_data['args'].rstrip())
-            if len(paths) > 2:
-                print "Usage: upload <local_file> <remote_dir>"
+            if len(paths) < 2:
+                print "Usage: upload <local_path> <remote_path>"
                 return
+            
+            if len(paths) > 2:
+                print "Usage: upload <local_path> <remote_path>"
+                return
+            
+            w = os.environ['OLDPWD']
+            os.chdir(w)
             
             local_dir = os.path.split(paths[0])[0]
             local_file = os.path.split(paths[0])[1]
             
-            if len(paths) == 1:
-                remote_dir = "."
-                remote_file = local_file
+            remote_dir = os.path.split(paths[1])[0]
+            remote_file = os.path.split(paths[1])[1]
+                
+            if os.path.exists(paths[0]):
+                pass
             else:
-                remote_dir = os.path.split(paths[1])[0]
-                if not remote_dir:
-                    remote_dir = "."
-                remote_file = os.path.split(paths[1])[1]
-                if not remote_file:
-                    remote_file = local_file
-
-            w = os.environ['OLDPWD']
-            os.chdir(w)
-
-            session.upload_file(paths[0],remote_dir,remote_file)
-
+                h.info_error("Local file: "+paths[0]+": does not exist!")
+                g = os.environ['HOME']
+                os.chdir(g + "/mouse")
+                return
+            
+            raw = remote_dir + '/' + remote_file
+            payload = """if [[ -d """+raw+""" ]]
+            then
+            echo 0
+            fi"""
+            dchk = session.send_command({"cmd":"","args":payload})
+            chk = session.send_command({"cmd":"stat","args":raw})
+            if dchk == "0\n":
+                if chk[:4] != "stat":
+                        session.upload_file(paths[0],raw,local_file)
+                        h.info_success("File successfully uploaded!")
+                else:
+                    h.info_error("Remote directory: "+raw+": does not exist!")
+            else:
+               schk = session.send_command({"cmd":"stat","args":remote_dir})
+               if schk[:4] != "stat":
+                   session.upload_file(paths[0],remote_dir,remote_file)
+                   h.info_success("File successfully uploaded!")
+               else:
+                   h.info_error("Remote directory: "+remote_dir+": does not exist!")
+            
             g = os.environ['HOME']
             os.chdir(g + "/mouse")
-
-            h.info_general("File successfully uploaded!")
