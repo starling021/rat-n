1e332aca953bff9c8221c23314c4915783ebf8eb
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-07 22:40:02 +0300

    Update screenshot_macos.py

--- etc/core/commands/macos/screenshot_macos.py
+++ etc/core/commands/macos/screenshot_macos.py
@@ -38,47 +38,47 @@ class command:
 		dest = cmd_data['args'][0]
                 if os.path.isdir(dest):
                     if os.path.exists(dest):
-			 h.info_general("Taking screenshot...")
-			 result = json.loads(session.send_command(cmd_data))
-    			 if 'error' in result:
-    	    		     h.info_error(result['error'])
-            		     return
-    			 elif 'size' in result:
-	    		     size = int(result['size'])
-	    		     data = session.sock_receive_data(size)
-	    		     f = open(os.path.join(dest,'screenshot.jpg'),'w')
-	    		     f.write(data)
-	    		     f.close()
-                         if dest[-1:] == "/":
-                             h.info_general("Saving to "+dest+"screenshot.jpg...")
-                             time.sleep(1)
-                             h.info_success("Saved to "+dest+"screenshot.jpg!")
-                         else:
-                             h.info_general("Saving to "+dest+"/screenshot.jpg...")
-                             time.sleep(1)
-                             h.info_success("Saved to "+dest+"/screenshot.jpg!")
-                    else:
-                        h.info_error("Local directory: "+dest+": does not exist!")
-                else:
-                    rp = os.path.split(dest)[0]
-                    if os.path.exists(rp):
-			if os.path.isdir(rp):
-			    pr = os.path.split(dest)[0]
-                            rp = os.path.split(dest)[1]
-			    h.info_general("Taking screenshot...")
-			    result = json.loads(session.send_command(cmd_data))
-    			    if 'error' in result:
-    	    		        h.info_error(result['error'])
-            		        return
-    			    elif 'size' in result:
-	    		        size = int(result['size'])
-	    		        data = session.sock_receive_data(size)
-	    		        f = open(os.path.join(pr,rp),'w')
-	    		        f.write(data)
-	    		        f.close()
-                            h.info_general("Saving to "+dest+"...")
+			h.info_general("Taking screenshot...")
+			result = json.loads(session.send_command(cmd_data))
+    			if 'error' in result:
+    	    		    h.info_error(result['error'])
+            		    return
+    			elif 'size' in result:
+	    		    size = int(result['size'])
+	    		    data = session.sock_receive_data(size)
+	    		    f = open(os.path.join(dest,'screenshot.jpg'),'w')
+	    		    f.write(data)
+	    		    f.close()
+                        if dest[-1:] == "/":
+                            h.info_general("Saving to "+dest+"screenshot.jpg...")
                             time.sleep(1)
-                            h.info_success("Saved to "+dest+"!")
+                            h.info_success("Saved to "+dest+"screenshot.jpg!")
+                        else:
+                            h.info_general("Saving to "+dest+"/screenshot.jpg...")
+                            time.sleep(1)
+                            h.info_success("Saved to "+dest+"/screenshot.jpg!")
+                   else:
+                       h.info_error("Local directory: "+dest+": does not exist!")
+               else:
+                   rp = os.path.split(dest)[0]
+                   if os.path.exists(rp):
+		       if os.path.isdir(rp):
+			   pr = os.path.split(dest)[0]
+                           rp = os.path.split(dest)[1]
+			   h.info_general("Taking screenshot...")
+			   result = json.loads(session.send_command(cmd_data))
+    			   if 'error' in result:
+    	    		       h.info_error(result['error'])
+            		       return
+    			   elif 'size' in result:
+	    		       size = int(result['size'])
+	    		       data = session.sock_receive_data(size)
+	    		       f = open(os.path.join(pr,rp),'w')
+	    		       f.write(data)
+	    		       f.close()
+                           h.info_general("Saving to "+dest+"...")
+                           time.sleep(1)
+                           h.info_success("Saved to "+dest+"!")
                         else:
                             h.info_error("Error: "+rp+": not a directory!")
                     else:
