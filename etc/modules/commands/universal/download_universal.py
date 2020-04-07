02ed3256f0b1a5a68effb7cdad428b75e272c79c
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-07 14:12:18 +0300

    Update download_universal.py

--- etc/modules/commands/universal/download_universal.py
+++ etc/modules/commands/universal/download_universal.py
@@ -27,25 +27,58 @@ class command:
 	def __init__(self):
 		self.name = "download"
 		self.description = "Download remote file."
-		self.usage = "Usage: download <remote_path>"
+		self.usage = "Usage: download <remote_path> <local_path>"
 		self.type = "native"
 
 	def run(self,session,cmd_data):
-		if not cmd_data['args']:
+		if len(cmd_data['args'].split()) < 2:
             		print self.usage
             		return
 		
-		w = os.environ['OLDPWD']
-            	os.chdir(w)
-		remote_path = os.path.split(cmd_data['args'])[-1]
-		data = session.download_file(cmd_data['args'])
-		h.info_general("Downloading {0}...".format(remote_path))
-		if data:
-			# save to downloads
-			h.info_general("Saving {0}...".format(remote_path))
-			f = open(os.path.join('downloads',remote_path),'w')
-			f.write(data)
-			f.close()
-			h.info_success("Saved to downloads/"+remote_path+"!")
-		g = os.environ['HOME']
-            	os.chdir(g + "/mouse")
+		payload = """if [[ -d """+cmd_data['args'].split()[0]+"""
+		then
+		echo 0
+		fi"""
+		dchk = session.send_command({"cmd":"","args":payload})
+		chk = session.send_command({"cmd":"stat","args":cmd_data['args'].split()[0]})
+                if chk[:4] != "stat":
+		    if dchk == "0\n":
+			h.info_error("Error: "+cmd_data['args'].split()[0]+": not a file!")
+		    else:
+		        if os.path.isdir(cmd_data['args'].split()[1]):
+		    	    if os.path.exists(cmd_data['args'].split()[1]):
+			        rp = os.path.split(cmd_data['args'].split()[0])[1]
+			        data = session.download_file(cmd_data['args'].split()[0])
+			        h.info_general("Downloading {0}...".format(rp))
+			        if data:
+			            h.info_general("Saving to "+cmd_data['args']+"/{0}...".format(rp))
+			            f = open(os.path.join(cmd_data['args'].split()[1],rp),'w')
+			            f.write(data)
+			            f.close()
+                                    if cmd_data['args'].split()[1][-1:] == "/":
+                                        h.info_success("Saved to "+cmd_data['args'].split()[1]+""+rp+"!")
+                                    else:
+			                h.info_success("Saved to "+cmd_data['args'].split()[1]+"/"+rp+"!")
+                            else:
+                                h.info_error("Local directory: "+cmd_data['args'].split()[1]+": does not exist!")
+			else:
+			    rp = os.path.split(cmd_data['args'].split()[1])[0]
+                            if os.path.exists(rp):
+			        if os.path.isdir(rp):
+				    prr = os.path.split(cmd_data['args'].split()[1])[0]
+				    rp = os.path.split(cmd_data['args'].split()[1])[1]
+				    pr = os.path.split(cmd_data['args'].split()[0])[1]
+			    	    data = session.download_file(cmd_data['args'].split()[0])
+			    	    h.info_general("Downloading {0}...".format(pr))
+			    	    if data:
+			                h.info_general("Saving to {0}...".format(cmd_data['args'].split()[1]))
+			                f = open(os.path.join(prr,rp),'w')
+			                f.write(data)
+			                f.close()
+			                h.info_success("Saved to "+cmd_data['args'].split()[1]+"!")
+                                else:
+				    h.info_error("Error: "+rp+": not a directory!")
+                   	    else:
+			        h.info_error("Local directory: "+rp+": does not exists!")
+                else:
+		    h.info_error("Remote file: "+cmd_data['args'].split()[0]+": does not exist!")
