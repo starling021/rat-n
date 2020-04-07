81ad7aa9a80c5b822464f05429e3d96315729a0e
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-07 22:19:40 +0300

    Update download_universal.py

--- etc/core/commands/universal/download_universal.py
+++ etc/core/commands/universal/download_universal.py
@@ -35,6 +35,8 @@ class command:
             		print self.usage
             		return
 		
+		w = os.environ['OLDPWD']
+                os.chdir(w)
 		payload = """if [[ -d """+cmd_data['args'].split()[0]+"""
 		then
 		echo 0
@@ -82,3 +84,5 @@ class command:
 			        h.info_error("Local directory: "+rp+": does not exists!")
                 else:
 		    h.info_error("Remote file: "+cmd_data['args'].split()[0]+": does not exist!")
+		g = os.environ['HOME']
+                os.chdir(g + "/mouse")
