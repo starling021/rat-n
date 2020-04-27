8066292cc31bf2824b8e3966c934c7632c7b8c8b
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-27 09:59:18 +0300

    Update duck_macos_payload.py

--- core/payloads/duck_macos_payload.py
+++ core/payloads/duck_macos_payload.py
@@ -50,7 +50,8 @@ class payload:
                         else:
                              payload_save_path = path + "/payload.txt"
 		    else:
-			h.info_error("Local directory: "+dest+": does not exist!")
+			h.info_error("Local directory: "+path+": does not exist!")
+			exit
 		else:
 		    direct = os.path.split(path)[0]
 		    if direct != "":
