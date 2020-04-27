803bebcf360858234e506a5aca932f3479cf7941
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-27 10:12:42 +0300

    Update app_macos_payload.py

--- core/payloads/app_macos_payload.py
+++ core/payloads/app_macos_payload.py
@@ -57,18 +57,19 @@ class payload:
 			exit
 		else:
 		    direct = os.path.split(path)[0]
-		    if direct != "":
-		        if os.path.exists(direct):
-		            if os.path.isdir(direct):
-		                payload_save_path = path
-		            else:
-			        h.info_error("Error: "+direct+": not a directory!")
-			        exit
-		        else:
-		            h.info_error("Local directory: "+direct+": does not exist!")
-		            exit
+		    if direct == "":
+			direct = "."
 		    else:
-			payload_save_path = path
+			pass
+		    if os.path.exists(direct):
+		        if os.path.isdir(direct):
+		            payload_save_path = path
+		        else:
+			    h.info_error("Error: "+direct+": not a directory!")
+			    exit
+		    else:
+		        h.info_error("Local directory: "+direct+": does not exist!")
+		        exit
 		os.system("cp -r data/app/payload.app "+path+" > /dev/null")
 		os.system("mv "+icon+" "+path+"/Contents/Resources/payload.icns > /dev/null")
 		payload = """\
