68dff30e9eb4b17d3b31d477fb6954e60f737b24
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-07 11:59:10 +0300

    Update alert_macos.py

--- core/commands/macos/alert_macos.py
+++ core/commands/macos/alert_macos.py
@@ -26,29 +26,34 @@ class command:
     def __init__(self):
         self.name = "alert"
         self.description = "Make alert show up on device."
+        self.usage = "Usage: <title> <message> <icon> <application> <first_button> <second_button>"
         self.type = "applescript"
 
     def run(self,session,cmd_data):
-        title = raw_input(h.WW+"[>]"+h.WHITE+" Alert Title: "+h.ENDC).strip(" ")
-        message = raw_input(h.WW+"[>]"+h.WHITE+" Alert Message: "+h.ENDC).strip(" ")
-        icon = raw_input(h.WW+"[>]"+h.WHITE+" Alert Icon: "+h.ENDC).strip(" ")
-        application = raw_input(h.WW+"[>]"+h.WHITE+" Alert Application: "+h.ENDC).strip(" ")
-        fbutton = raw_input(h.WW+"[>]"+h.WHITE+" First Button: "+h.ENDC).strip(" ")
-        sbutton = raw_input(h.WW+"[>]"+h.WHITE+" Second Button: "+h.ENDC).strip(" ")
-        one = '"'
-        payload = """
-        tell application """+one+""""""+application+""""""+one+"""
-            activate
+        cmds = cmd_data['args'].split()
+        if len(cmds) < 6:
+            print self.usage
+        else:
+            title = cmds[0]
+            message = cmds[1]
+            icon = cmds[2]
+            application = cmds[3]
+            fbutton = cmds[4]
+            sbutton = cmds[5]
+            one = '"'
+            payload = """
+            tell application """+one+""""""+application+""""""+one+"""
+                activate
             
-            set theAlertText to "An error has occurred."
-            set theAlertMessage to "The amount of available free space is dangerously low. Would you like to continue?"
+                set theAlertText to "An error has occurred."
+                set theAlertMessage to "The amount of available free space is dangerously low. Would you like to continue?"
             
-                try
-                    display dialog """+one+""""""+message+""""""+one+""" with title """+one+""""""+title+""""""+one+""" buttons {"""+one+""""""+fbutton+""""""+one+""", """+one+""""""+sbutton+""""""+one+"""} default button """+one+""""""+fbutton+""""""+one+""" cancel button """+one+""""""+sbutton+""""""+one+""" with icon path to resource """+one+""""""+icon+""".icns"""+one+""" in bundle "/System/Library/CoreServices/CoreTypes.bundle"
-                end try
+                    try
+                        display dialog """+one+""""""+message+""""""+one+""" with title """+one+""""""+title+""""""+one+""" buttons {"""+one+""""""+fbutton+""""""+one+""", """+one+""""""+sbutton+""""""+one+"""} default button """+one+""""""+fbutton+""""""+one+""" cancel button """+one+""""""+sbutton+""""""+one+""" with icon path to resource """+one+""""""+icon+""".icns"""+one+""" in bundle "/System/Library/CoreServices/CoreTypes.bundle"
+                    end try
                         
-        end tell        
-        """
-        cmd_data.update({"cmd":"applescript","args":payload})
-        alert = session.send_command(cmd_data).strip()
-        return ""
+            end tell        
+            """
+            cmd_data.update({"cmd":"applescript","args":payload})
+            alert = session.send_command(cmd_data).strip()
+            return ""
