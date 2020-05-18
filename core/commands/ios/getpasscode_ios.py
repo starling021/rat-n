f41aaf24d3c6cc13b65d77bba07208579ad38093
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-18 20:00:49 +0300

    Update getpasscode_ios.py

--- core/commands/ios/getpasscode_ios.py
+++ core/commands/ios/getpasscode_ios.py
@@ -22,7 +22,7 @@ import core.helper as h
 
 class command:
     def __init__(self):
-        self.name = "getpass"
+        self.name = "getpasscode"
         self.description = "Retreive the device passcode."
     
     def run(self,session,cmd_data):
