4eae2f4eb812ad82cafa5c67b48414d2f3d8e807
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-19 10:30:49 +0300

    Update getvol_macos.py

--- core/commands/macos/getvol_macos.py
+++ core/commands/macos/getvol_macos.py
@@ -21,11 +21,11 @@
 import json
 class command:
     def __init__(self):
-        self.name = "getvol"
-        self.description = "Get speaker output volume."
+        self.name = "volume"
+        self.description = "Show volume level."
         self.type = "applescript"
 
     def run(self,session,cmd_data):
         payload = "output volume of (get volume settings)"
         cmd_data.update({"cmd":"applescript","args":payload})
-        print("Current Volume: "+session.send_command(cmd_data).decode())
+        print("Volume Level: "+session.send_command(cmd_data).decode())
