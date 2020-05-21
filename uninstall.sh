01fce43c3f6270ad5eaff9e5ce9e0d367ce91294
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-21 17:54:45 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -18,16 +18,15 @@
 #        You should have received a copy of the GNU General Public License
 #        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-RS="\033[1;31m"
-YS="\033[1;33m"
-CE="\033[0m"
-WHS="\033[0m"
-
 printf '\033]2;uninstall.sh\a'
+
+G="\033[1;34m[*] \033[0m"
+S="\033[1;32m[+] \033[0m"
+E="\033[1;31m[-] \033[0m"
 
 if [[ $EUID -ne 0 ]]
 then
-   echo -e ""$RS"[-]"$WHS" This script must be run as root!"$CE""
+   echo -e ""$E"This script must be run as root!"
    exit
 fi
 
