a3f0b3dcd4ac252989631319f5311602564fe337
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-06-04 13:27:13 +0300

    Update lastapp_ios.py

--- core/commands/ios/lastapp_ios.py
+++ core/commands/ios/lastapp_ios.py
@@ -23,7 +23,7 @@ import core.helper as h
 class command:
     def __init__(self):
         self.name = "lastapp"
-        self.description = "Get last opened application."
+        self.description = "Show last opened application."
     
     def run(self,session,cmd_data):
         h.info_info("Last application: " + session.send_command(cmd_data).decode())
