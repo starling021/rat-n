f40ae0a7f19d9d6738ac976cf941028bcc703c29
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-06-04 13:25:24 +0300

    Update lastapp_ios.py

--- core/commands/ios/lastapp_ios.py
+++ core/commands/ios/lastapp_ios.py
@@ -18,10 +18,12 @@
 #        You should have received a copy of the GNU General Public License
 #        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import core.helper as h
+
 class command:
     def __init__(self):
         self.name = "lastapp"
         self.description = "Get last opened application."
     
     def run(self,session,cmd_data):
-        print(session.send_command(cmd_data).decode())
+        h.info_info("Last application: " + session.send_command(cmd_data).decode())
